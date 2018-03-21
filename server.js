655007639bb8f777d15027271995d3e71bdf5c0d
Michael Johnson <migarjo@github.com>
2018-03-21 11:01:04 -0500

    Add forever to start script

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-require('coffee-script/register');
-module.exports = require('hubot/bin/hubot.coffee');  
+var exec = require('child_process').exec, child;
+
+child = exec('forever start --uid ""universe-bot"" --verbose --append -l ""\D\home\site\wwwroot\universe-bot.log"" --sourceDir ""\D\home\site\wwwroot\"" --workingDir ""\D\home\site\wwwroot\"" --minUptime 100 --spinSleepTime 100 .\node_modules\coffee-script\bin\coffee .\node_modules\hubot\bin\hubot',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+ child();
